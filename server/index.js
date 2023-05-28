554f3aed4ce8f2049e38b2e7753c84bb33988522
Nishat-Tasniya <122631608+Nishat-Tasniya@users.noreply.github.com>
2023-05-28 21:57:33 +0600

    2nd

--- server/index.js
+++ server/index.js
@@ -9,12 +9,17 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import userRouter from "./routes/users.js"
-import { register } from "./controllers/auth";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 
-/* CONFIGURATION */
+/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -39,12 +44,14 @@ const storage = multer.diskStorage({
   });
   const upload = multer({ storage });
 
-/* ROUTES WITH FILES */
+  /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-//app.post("/posts", verifyToken, upload.single("picture"), createPost);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-////////   Routes ///////////
+/* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
@@ -55,5 +62,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(`${error} did not connect`));  
--- server/models/User.js
+++ server/models/User.js
@@ -9,38 +9,37 @@ const UserSchema = new mongoose.Schema(
       max: 50,
     },
     lastName: {
-        type: String,
-        required: true,
-        min: 2,
-        max: 50, 
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
     },
     email: {
-        type: String,
-        required: true,
-        max: 50, 
-        unique: true,
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-        min: 5,
+      type: String,
+      required: true,
+      min: 5,
     },
     picturePath: {
-        type: String,
-        default:"",
+      type: String,
+      default: "",
     },
     friends: {
-        type: Array,
-        default: []
+      type: Array,
+      default: [],
     },
     location: String,
     occupation: String,
     viewedProfile: Number,
     impressions: Number,
   },
-  {timestamps: true}
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", UserSchema);
-
 export default User;
